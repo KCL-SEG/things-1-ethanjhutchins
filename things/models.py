9bd98e6f1350fd6377df194aaf98e76d3f62d124
Ethan Hutchins <ethanjameshutchins@gmail.com>
2022-12-04 15:02:23 +0000

    Fix 'Things' model.

--- things/models.py
+++ things/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField()
-    description = models.TextField()
-    quantity = models.PositiveIntegerField()
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.TextField(max_length=120, blank=True, unique=False)
+    quantity = models.PositiveIntegerField(unique=False)
