57c6eb74edbf96a6754534ebb417d57bbb9e0f46
Ethan Hutchins <ethanjameshutchins@gmail.com>
2022-12-04 15:13:43 +0000

    Fix max length of description field.

--- things/models.py
+++ things/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator, MaxLengthValidator
 
 # Create your models here.
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
-    description = models.TextField(max_length=120, blank=True, unique=False)
+    description = models.TextField(max_length=120, blank=True, unique=False, validators=[MaxLengthValidator(120)])
     quantity = models.PositiveIntegerField(unique=False, blank=False, validators= [
         MaxValueValidator(100), MinValueValidator(0)
     ])
